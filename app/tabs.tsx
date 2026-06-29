dfbf3c9faa1fa37ddd69ebc74432c359a2ef5a57
Mitchell Wilson <mitchellwilson@Mitchells-MacBook-Pro.local>
2026-06-29 16:12:01 -0500

    feat: add debounced search input to home page

---
+++ app/tabs.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Tabs(tabs) {
+
+
+    const [activeTab, setActiveTab] = React.useState(0);
+
+    return (
+        <Tabs>
+          { children }
+     </Tabs>
+    )
+}
